bbc008e45bc346bd4a4019ade27ef24148875a1c
joseporresD <jose.porres@utec.edu.pe>
2023-09-22 10:11:11 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Monthly taxes lab
---
+++ backend/pkg/monthlytaxes/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func GetMonthlyTaxes(dni string) string {
+	// call public api
+	// https://api.sunat.cloud/ruc/{dni}
+	apiUrl := "https://api.sunat.cloud/ruc/" + dni
+	req, _ := http.NewRequest("GET", apiUrl, nil)
+	if req != nil {
+		fmt.Println("Error al llamar a la API")
+		return "Error al llamar a la API"
+	}
+	fmt.Println(req)
+	return "OK"
+}
+
+
+func GetDNI(w http.ResponseWriter, r *http.Request) {
+	// Parse the DNI parameter from the URL
+	vars := mux.Vars(r)
+	dni := vars["dni"]
+
+	// call sunat api
+	response := GetMonthlyTaxes(dni)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, response)
+}
+
+func main() {
+	// Create a new router instance using the Gorilla Mux router
+	router := mux.NewRouter()
+
+	// Define a route for the API endpoint that accepts a DNI parameter in the URL
+	router.HandleFunc("/monthly-taxes/{dni}", GetDNI).Methods("GET")
+
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is running on :8080")
+	http.Handle("/", router)
+	http.ListenAndServe(":8080", nil)
+}
